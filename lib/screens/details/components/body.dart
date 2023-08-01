31df64a22e10a7733eec38ed23245283dac5fcaf
ten66 <ikesho.1217@gmail.com>
2023-08-01 22:25:02 +0900

    #3 detail pageの下部のtextbutton2つを追加

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
 import 'image_and_icons.dart';
 import 'title_and_price.dart';
 
@@ -8,15 +9,47 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        ImageAndIcons(size: size),
-        const TitleAndPrice(
-          title: 'Angelica',
-          country: 'Russia',
-          price: 440,
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ImageAndIcons(size: size),
+          const TitleAndPrice(title: 'Angelica', country: 'Russia', price: 440),
+          const SizedBox(height: kDefaultPadding),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                // error - A RenderFlex overflowed by 23 pixels on the bottom.
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Buy Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Description'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
