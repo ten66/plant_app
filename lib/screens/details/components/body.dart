bac8b35159ed2b41514f975f1d549d5970c1d4f2
ten66 <ikesho.1217@gmail.com>
2023-08-01 17:59:49 +0900

    #3 back arrowボタンの位置調整

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -10,45 +10,57 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  IconButton(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: SvgPicture.asset('assets/icons/back_arrow.svg'),
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 3,
                   ),
-                ],
-              ),
-            ),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 10),
-                    blurRadius: 60,
-                    color: kPrimaryColor.withOpacity(0.29),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: kDefaultPadding),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: SvgPicture.asset('assets/icons/back_arrow.svg'),
+                        ),
+                      ),
+                      const Spacer(),
+                    ],
                   ),
-                ],
-                image: const DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/img.png'),
                 ),
               ),
-            ),
-          ],
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(63),
+                    bottomLeft: Radius.circular(63),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: const Offset(0, 10),
+                      blurRadius: 60,
+                      color: kPrimaryColor.withOpacity(0.29),
+                    ),
+                  ],
+                  image: const DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/img.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
