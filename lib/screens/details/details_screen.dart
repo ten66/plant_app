2ca7c6ec1dba0086f4d8e49383cb4709cfddd05e
ten66 <ikesho.1217@gmail.com>
2023-08-01 16:06:20 +0900

    #3 詳細ページのファイル作成

---
+++ lib/screens/details/details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/screens/home/components/body.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
