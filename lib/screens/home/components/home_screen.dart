a165c467068c60a5ee1b9584283280f2d876aa52
ten66 <ikesho.1217@gmail.com>
2023-08-01 15:48:43 +0900

    #1 微修正

--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:plant_app/constants.dart';
+// import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/body.dart';
 
 import '../../../components/my_bottom_nav_bar.dart';
