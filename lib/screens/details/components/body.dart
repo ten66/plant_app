254e4b8e18b51522512bf7c9bf99301128b5ac88
ten66 <ikesho.1217@gmail.com>
2023-08-01 21:39:27 +0900

    #3 create image and icons widget

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:plant_app/constants.dart';
-
-import 'icon_card.dart';
+import 'image_and_icons.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -12,62 +9,7 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        SizedBox(
-          height: size.height * 0.8,
-          child: Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: kDefaultPadding * 3,
-                  ),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: IconButton(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: kDefaultPadding),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: SvgPicture.asset('assets/icons/back_arrow.svg'),
-                        ),
-                      ),
-                      const Spacer(),
-                      const IconCard(icon: 'assets/icons/sun.svg'),
-                      const IconCard(icon: 'assets/icons/icon_2.svg'),
-                      const IconCard(icon: 'assets/icons/icon_3.svg'),
-                      const IconCard(icon: 'assets/icons/icon_4.svg'),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: size.height * 0.8,
-                width: size.width * 0.75,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(63),
-                    bottomLeft: Radius.circular(63),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, 10),
-                      blurRadius: 60,
-                      color: kPrimaryColor.withOpacity(0.29),
-                    ),
-                  ],
-                  image: const DecorationImage(
-                    alignment: Alignment.centerLeft,
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/images/img.png'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        ImageAndIcons(size: size),
       ],
     );
   }
