a30d18efc50fa945af49325a0daf49b77ab5bb8c
ten66 <ikesho.1217@gmail.com>
2023-08-01 22:29:12 +0900

    #3 buynowとdescriptionボタンのWidgetを別ファイルに作成

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
+import 'buynow_and_description_btn.dart';
 import 'image_and_icons.dart';
 import 'title_and_price.dart';
 
@@ -15,39 +16,7 @@ class Body extends StatelessWidget {
           ImageAndIcons(size: size),
           const TitleAndPrice(title: 'Angelica', country: 'Russia', price: 440),
           const SizedBox(height: kDefaultPadding),
-          Row(
-            children: [
-              SizedBox(
-                width: size.width / 2,
-                // error - A RenderFlex overflowed by 23 pixels on the bottom.
-                height: 84,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: kPrimaryColor,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20),
-                      ),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Buy Now',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text('Description'),
-                ),
-              ),
-            ],
-          ),
+          BuyNowAndDescriptionBtn(size: size),
         ],
       ),
     );
